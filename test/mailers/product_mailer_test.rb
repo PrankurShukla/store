784e42832d1b13607d3c11cd34f11c0eafb630a8
PrankurShukla <shuklaprankur27@gmail.com>
2025-08-22 17:19:50 +0530

    add test file

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -10,7 +10,7 @@ class ProductMailerTest < ActionMailer::TestCase
     # Make sure these match your actual mailer values
     assert_equal "#{product.name} is back in stock!", mail.subject
     assert_equal [ subscriber.email ], mail.to
-    assert_equal [ "shuklaprankur27@gmail.com" ], mail.from  # Update this to match your mailer default
+    assert_equal [ "no-reply@yourdomain.com" ], mail.from  # Update this to match your mailer default
 
     assert_match "Good news!", mail.body.encoded
     assert_match product.name, mail.body.encoded
